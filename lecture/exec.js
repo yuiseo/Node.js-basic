cfdd59fa6e6470d724fa34c7774c8ba59f67a044
유이서 <yoojh456@naver.com>
2023-04-10 22:54:25 +0900

    exec-dir

---
+++ lecture/exec.js
@@ -0,0 +1,11 @@
+const exec = require('child_process').exec
+
+var process = exec('dir')
+
+process.stdout.on('data',function(data){
+  console.log(data.toString())
+})
+
+process.stderr.on('data',function(data){
+  console.log(data.toString())
+})
