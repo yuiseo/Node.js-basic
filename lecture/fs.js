148817ae384e5f453bb4b1f2621c0d6fa1a4275a
유이서 <yoojh456@naver.com>
2023-04-11 00:03:56 +0900

    from callbak function to Promises

--- lecture/fs.js
+++ lecture/fs.js
@@ -1,9 +1,10 @@
-const fs = require('fs')
+const fs = require('fs').promises
 
-fs.readFile('./readme.txt',(err,data)=>{
-  if(err){
+fs.readFile('./readme.txt')
+  .then((data)=>{
+    console.log(data)
+    console.log(data.toString())
+  })
+  .catch((err)=>{
     throw err
-  }
-  console.log(data)
-  console.log(data.toString())
-})
+  })
